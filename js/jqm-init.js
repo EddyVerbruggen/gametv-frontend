3d466f07c6602173705815a56f414005634105e9
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-12-28 20:55:30 +0100

    tweaks

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,4 +1,5 @@
 // override some jqm defaults, need to be done before jqm is loaded
 $(document).bind("mobileinit", function(){
+  $.mobile.page.prototype.options.domCache = true;
   $.mobile.pushStateEnabled = false;
 });
