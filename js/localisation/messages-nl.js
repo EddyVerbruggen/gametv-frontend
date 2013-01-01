814d56d387f52e1598591a428f69940c8cbeead8
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-01-01 19:27:20 +0100

    localisation

---
+++ js/localisation/messages-nl.js
@@ -0,0 +1,16 @@
+button_back = 'terug';
+
+menu_option_editvideosources = 'Kanalen aan / uit';
+menu_option_about = 'Over Game TV';
+menu_option_close = 'Sluiten';
+
+page_home_channellist_header = 'Kies een recent geupdate kanaal';
+
+page_editvideosources_title = 'Beheer kanalen';
+page_editvideosources_button_cancel = 'annuleer';
+page_editvideosources_button_save = 'Opslaan';
+page_editvideosources_switch_on = 'aan';
+page_editvideosources_switch_off = 'uit';
+
+page_share_title = 'Mail een vriend';
+page_share_button = 'Naar verzenden';
