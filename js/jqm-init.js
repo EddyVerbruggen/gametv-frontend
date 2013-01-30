cd6f63cbc2d4d9b259df983f6bf788b610ad03f1
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-01-30 19:14:35 +0100

    Fixed bug for sources with spaces and periods

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,6 +1,6 @@
 // override some jqm defaults, need to be done before jqm is loaded
 $(document).bind("mobileinit", function(){
-  $.mobile.page.prototype.options.domCache = false;
+  $.mobile.page.prototype.options.domCache = true;
   $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "slidefade" : "slide";
 //  $.mobile.defaultPageTransition = isAndroid() ? "fade" : "slide";
