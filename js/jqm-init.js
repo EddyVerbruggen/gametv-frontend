337ea7a5b7645337c8c2dc38cfc4a116f1e5e51a
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-01-30 16:27:34 +0100

    Testing domcache off, for a user with navigation issues

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,9 +1,9 @@
 // override some jqm defaults, need to be done before jqm is loaded
 $(document).bind("mobileinit", function(){
-  $.mobile.page.prototype.options.domCache = true;
+  $.mobile.page.prototype.options.domCache = false;
   $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "slidefade" : "slide";
 //  $.mobile.defaultPageTransition = isAndroid() ? "fade" : "slide";
   $.mobile.defaultPageTransition = "slide";
-  $.event.special.swipe.horizontalDistanceThreshold = 60; // default is 30, but swipe is triggered too often for my taste
+  $.event.special.swipe.horizontalDistanceThreshold = 80; // default is 30, but swipe is triggered too often for my taste
 });
