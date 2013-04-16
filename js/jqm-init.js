1de2c0f828ba906bca2a1798fc6cb9c0dcd53d16
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-04-16 23:09:36 +0200

    Layout tweaks

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,10 +1,10 @@
 // override some jqm defaults, need to be done before jqm is loaded
 $(document).bind("mobileinit", function(){
-  $.mobile.page.prototype.options.domCache = true;
-  $.mobile.pushStateEnabled = false;
+//  $.mobile.page.prototype.options.domCache = true;
+//  $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "slidefade" : "slide";
 //  $.mobile.defaultPageTransition = isAndroid() ? "fade" : "slide";
   $.mobile.defaultPageTransition = "slide";
-  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
-  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
+//  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
+//  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
 });
