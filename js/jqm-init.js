a339abcf27fe9d311a40c3065f985eff35275fdd
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-04-16 23:38:16 +0200

    Layout tweaks

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,7 +1,7 @@
 // override some jqm defaults, need to be done before jqm is loaded
 $(document).bind("mobileinit", function(){
-//  $.mobile.page.prototype.options.domCache = true;
-  $.mobile.pushStateEnabled = false;
+  $.mobile.page.prototype.options.domCache = true;
+//  $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "slidefade" : "slide";
 //  $.mobile.defaultPageTransition = isAndroid() ? "fade" : "slide";
   $.mobile.defaultPageTransition = "slide";
