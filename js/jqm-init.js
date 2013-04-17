bb5a66f4edb733ae5046b10184a5a38ba832c807
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-04-17 13:31:29 +0200

    Transition fix?

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -2,9 +2,8 @@
 $(document).bind("mobileinit", function(){
   $.mobile.page.prototype.options.domCache = true;
 //  $.mobile.pushStateEnabled = false;
-//  $.mobile.defaultPageTransition = isAndroid() ? "slidefade" : "slide";
-//  $.mobile.defaultPageTransition = isAndroid() ? "fade" : "slide";
-  $.mobile.defaultPageTransition = "slide";
+  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
+//  $.mobile.defaultPageTransition = "slide";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
 });
