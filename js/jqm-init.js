964f3630b8ed66549b2a3475a8418cbaf27c7512
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-04-29 21:05:15 +0200

    broken..

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -7,11 +7,11 @@ $(document).bind("mobileinit", function(){
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
 
-  $(document).ajaxStart(function() {
-    $.mobile.loading('show');
-  });
+//  $(document).ajaxStart(function() {
+//    $.mobile.loading('show');
+//  });
 
-  $(document).ajaxStop(function() {
-    $.mobile.loading('hide');
-  });
+//  $(document).ajaxStop(function() {
+//    $.mobile.loading('hide');
+//  });
 });
