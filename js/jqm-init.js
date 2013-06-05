fb6c71eb17565e001f3737a2581d0439c32ef594
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-06-05 10:27:58 +0200

    - slides for ios - removed the clickstate

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -2,8 +2,8 @@
 $(document).bind("mobileinit", function(){
   $.mobile.page.prototype.options.domCache = true;
 //  $.mobile.pushStateEnabled = false;
-  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
-//  $.mobile.defaultPageTransition = "none";
+//  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
+  $.mobile.defaultPageTransition = "none";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
 
