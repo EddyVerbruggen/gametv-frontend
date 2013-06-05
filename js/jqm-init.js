5349ea3f0018bf60800a6d9eb7e6f0d0b39acd22
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-06-05 10:22:47 +0200

    - slides for ios - removed the clickstate

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -2,8 +2,8 @@
 $(document).bind("mobileinit", function(){
   $.mobile.page.prototype.options.domCache = true;
 //  $.mobile.pushStateEnabled = false;
-//  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
-  $.mobile.defaultPageTransition = "none";
+  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
+//  $.mobile.defaultPageTransition = "none";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
 
@@ -15,9 +15,6 @@ $(document).bind("mobileinit", function(){
 //    $.mobile.loading('hide');
 //  });
 
-  // remove any clicked states in header buttons (using 'on', so the function will attach to anything that's added to the DOM)
-  $(document).on("click", ".ui-header .ui-btn", function() {
-    $(this).removeClass("ui-btn-active");
-  });
-
+  // remove any clicked states in header buttons
+  $.mobile.activeBtnClass = "unused";
 });
