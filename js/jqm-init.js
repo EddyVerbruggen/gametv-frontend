51a6f1b344f392193decb258049c5708645dac49
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-06-05 14:27:51 +0200

    cleanup

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -6,15 +6,6 @@ $(document).bind("mobileinit", function(){
   $.mobile.defaultPageTransition = "none";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
-
-  $(document).ajaxStart(function() {
-    $.mobile.loading('show');
-  });
-
-  $(document).ajaxStop(function() {
-    $.mobile.loading('hide');
-  });
-
   // remove any clicked states in header buttons
   $.mobile.activeBtnClass = "unused";
 });
