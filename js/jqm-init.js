6132e234c73b0eaf8fb340a25e0156accdac7d74
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-09-03 21:22:00 +0200

    JQuery Mobile 1.4 (alpha 2)

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,7 +1,7 @@
 // override some jqm defaults, need to be done before jqm is loaded
 $(document).bind("mobileinit", function(){
   $.mobile.page.prototype.options.domCache = true;
-//  $.mobile.pushStateEnabled = false;
+  $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
   $.mobile.defaultPageTransition = "none";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
