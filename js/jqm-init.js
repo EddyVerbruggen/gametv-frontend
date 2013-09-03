2eea38b3eaa629f99382af5f7b5510fc0104734e
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-09-03 21:53:04 +0200

    JQuery Mobile 1.4 (alpha 2) - beautification

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -3,7 +3,7 @@ $(document).bind("mobileinit", function(){
   $.mobile.page.prototype.options.domCache = true;
   $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
-  $.mobile.defaultPageTransition = "slide";
+  $.mobile.defaultPageTransition = "none";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
   // remove any clicked states in header buttons
