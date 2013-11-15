ac9a3ca3034323a93223153a1ac91fe76af56955
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-11-15 23:24:45 +0100

    - Truly fixed header - Re-intropduced slide animation (page transition)

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -3,7 +3,7 @@ $(document).bind("mobileinit", function(){
   $.mobile.page.prototype.options.domCache = true;
   $.mobile.pushStateEnabled = false;
 //  $.mobile.defaultPageTransition = isAndroid() ? "none" : "slide";
-  $.mobile.defaultPageTransition = "none";
+  $.mobile.defaultPageTransition = "slide";
 //  $.event.special.swipe.horizontalDistanceThreshold = 120; // default is 30, but swipe is triggered too often for my taste
 //  $.event.special.swipe.verticalDistanceThreshold = 100; // default is 30, but swipe is triggered too often for my taste
   // remove any clicked states in header buttons
