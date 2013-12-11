57720f47bcde28b3d71be989e5ec13ed8c2e2561
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-12-11 23:30:17 +0100

    testing..

---
+++ js/localisation/messages-nl-NL.js
@@ -0,0 +1,32 @@
+button_back = 'terug';
+
+country_nl = 'Nederland';
+country_be = 'Belgie';
+country_gb = 'Engeland';
+country_us = 'Verenigde Staten';
+country_fi = 'Finland';
+country_se = 'Zweden';
+
+month_3 = 'maa';
+month_5 = 'mei';
+
+page_home_button_refresh = 'ververs';
+page_home_button_menu = 'kanalen';
+
+menu_option_editvideosources = 'Kanalen aan / uit';
+menu_option_about = 'Over Game TV';
+menu_option_close = 'Sluiten';
+
+page_home_channellist_header = 'Kies een recent geupdate kanaal';
+
+page_editvideosources_title = 'Kies kanalen';
+page_editvideosources_button_cancel = 'annuleer';
+page_editvideosources_button_save = 'opslaan';
+page_editvideosources_checkbox_channel = 'Toon';
+
+page_share_title = 'Mail een vriend';
+page_share_button = 'Naar verzenden';
+
+notification_hide = 'Dit bericht verbergen';
+notification_item_homepage_pushconfighint = 'Heb je favoriete kanalen? Klik rechtsboven op &nbsp;<i class="icon-reorder"/>&nbsp; om een pushbericht in te stellen.';
+notification_item_homepage_begforreview_android = 'Tevreden met deze app? <a href="market://details?id=nl.x_services.gametv" onclick="hideNotification(\'homepage_begforreview_android\')"><i class="icon-star"/> Beoordeel Game TV <i class="icon-star"/></a> en geef tips voor verbeteringen in de comments. Bedankt!';
